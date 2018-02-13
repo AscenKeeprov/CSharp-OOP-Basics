2fa3855b09a49116ccc052b1713352b373859576
Ascen Keeprov <assenkiprov@yahoo.com>
2018-02-13 19:19:26 +0200

    Exercise1 upload

---
+++ Exercise1-DefiningClasses/DefiningClasses/Employee.cs
@@ -0,0 +1,23 @@
+﻿public class Employee
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+    public string Department { get; set; }
+    public string Position { get; set; }
+    public double Salary { get; set; }
+
+    public Employee()
+    {
+	Age = -1;
+	Email = "n/a";
+    }
+
+    public Employee(string name, double salary, string position, string department) : this()
+    {
+	Name = name;
+	Department = department;
+	Position = position;
+	Salary = salary;
+    }
+ }
