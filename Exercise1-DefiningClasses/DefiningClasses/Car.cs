269f49817971a733214da7d264366ca1cd15ca02
Ascen Keeprov <assenkiprov@yahoo.com>
2018-02-14 22:20:11 +0200

    Exercise1 upload

--- Exercise1-DefiningClasses/DefiningClasses/Car.cs
+++ Exercise1-DefiningClasses/DefiningClasses/Car.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 public class Car
 {
     public string Model { get; set; }
     public Engine Engine { get; set; }
-    public List<Tire> Tires { get; set; }
-    public Cargo Cargo { get; set; }
+    public int Weight { get; set; }
+    public string Color { get; set; }
     public double FuelAmount { get; set; }
     public double FuelConsumptionPerKm { get; set; }
     public double DistanceTraveled { get; set; }
@@ -17,12 +17,10 @@ public class Car
 	FuelConsumptionPerKm = fuelConsumptionPerKm;
     }
 
-    public Car(string model, Engine engine, List<Tire> tires, Cargo cargo)
+    public Car(string model, Engine engine)
     {
 	Model = model;
 	Engine = engine;
-	Tires = tires;
-	Cargo = cargo;
     }
 
     public Car TryDrive(Car car, double distance)
@@ -33,7 +31,19 @@ public class Car
 	    car.FuelAmount -= projectedFuelLoss;
 	    car.DistanceTraveled += distance;
 	}
-	else System.Console.WriteLine("Insufficient fuel for the drive");
+	else Console.WriteLine("Insufficient fuel for the drive");
 	return car;
+    }
+
+    public override string ToString()
+    {
+	string weight = "n/a";
+	if (Weight != 0) weight = Weight.ToString();
+	string color = "n/a";
+	if (Color != null) color = Color;
+	return $"{Model}:{Environment.NewLine}" +
+	    $"  {Engine.ToString()}{Environment.NewLine}" +
+	    $"  Weight: {weight}{Environment.NewLine}" +
+	    $"  Color: {color}";
     }
 }
--- Exercise1-DefiningClasses/DefiningClasses/Engine.cs
+++ Exercise1-DefiningClasses/DefiningClasses/Engine.cs
@@ -1,11 +1,34 @@
-﻿public class Engine
+﻿using System;
+
+public class Engine
 {
+    public string Model { get; set; }
     public int Power { get; set; }
     public int Speed { get; set; }
+    public int Displacement { get; set; }
+    public string Efficiency { get; set; }
 
     public Engine(int power, int speed)
     {
 	Power = power;
 	Speed = speed;
+    }
+
+    public Engine(string model, int power)
+    {
+	Model = model;
+	Power = power;
+    }
+
+    public override string ToString()
+    {
+	string displacement = "n/a";
+	if (Displacement != 0) displacement = Displacement.ToString();
+	string efficiency = "n/a";
+	if (Efficiency != null) efficiency = Efficiency;
+	return $"{Model}:{Environment.NewLine}" +
+	    $"    Power: {Power}{Environment.NewLine}" +
+	    $"    Displacement: {displacement}{Environment.NewLine}" +
+	    $"    Efficiency: {efficiency}";
     }
 }
