06055efa289b3c9f97c746860a982e6e248adb33
Ascen Keeprov <assenkiprov@yahoo.com>
2018-02-14 22:54:39 +0200

    Exercise1 upload

---
+++ Exercise1-DefiningClasses/DefiningClasses/Trainer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class Trainer
+{
+    public string Name { get; set; }
+    public int Badges { get; set; }
+    public List<Pokemon> Pokemons { get; set; }
+
+    public Trainer()
+    {
+	Pokemons = new List<Pokemon>();
+    }
+}
