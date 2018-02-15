1da156e30c139dd8ef6991344c463ba4bcb21bf1
Ascen Keeprov <assenkiprov@yahoo.com>
2018-02-15 18:34:39 +0200

    Exercise1 upload

---
+++ Exercise1-DefiningClasses/DefiningClasses/Breed.cs
@@ -0,0 +1,5 @@
+﻿public class Breed
+{
+    public string Name { get; set; }
+    public decimal Trait { get; set; }
+}
---
+++ Exercise1-DefiningClasses/DefiningClasses/Cat.cs
@@ -0,0 +1,12 @@
+﻿public class Cat
+{
+    public string Name { get; set; }
+    public Breed Breed { get; set; }
+
+    public override string ToString()
+    {
+	if (Breed.Name == "Cymric")
+	    return $"{Breed.Name} {Name} {Breed.Trait:F2}";
+	else return $"{Breed.Name} {Name} {Breed.Trait}";
+    }
+}
